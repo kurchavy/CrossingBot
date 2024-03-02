6a80fc3c58f8e80e0432645a2fae8d98f400c69b
kurchavy <kurchavy@gmail.com>
2024-03-02 23:53:41 +0300

    Added /users command to display bot users list

--- source/config_reader.py
+++ source/config_reader.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     bot_token: SecretStr
+    my_id: int
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
--- source/crossing_bot.py
+++ source/crossing_bot.py
@@ -30,11 +30,29 @@ async def cmd_start(message: types.Message):
     await message.answer(f"Для получения информации о текущем состоянии переезда используй команду /state")
 
 @dp.message(Command("state"))
-async def cmd_state(message: types.Message, crs: Crossing):
+async def cmd_state(message: types.Message, crs: Crossing, usr: dict[int, int]):
     resp = crs.get_current_state()
-    logging.debug(f'Request state from user {message.from_user.full_name}')
+    
+    user_key = f"{message.from_user.full_name}|{message.from_user.id}"
+    if usr.get(user_key) == None:
+        usr[user_key] = 0
+    usr[user_key] += 1
+
+    logging.debug(f'Request state from {user_key}')
     for line in resp:
         await message.answer(line)
+
+@dp.message(Command("users"))
+async def cmd_state(message: types.Message, usr: dict[int, int]):   
+    user_key = f"{message.from_user.full_name}|{message.from_user.id}"
+    logging.debug(f'Request users from {user_key}')
+    if message.from_user.id != config.my_id:
+        await message.answer(f"Информация недоступна")
+        return
+    await message.answer(f"Список пользователей бота (с момента перезапуска):")
+    for itm in usr.items():
+        await message.answer(f"{itm[0]} - {itm[1]} запр.")
+    
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
@@ -43,8 +61,10 @@ async def main():
     task_upd = asyncio.create_task(cu.update_task())
     logging.debug("Crossing infrastructure created, updater started")
 
+    users = {}
+
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, crs=c)
+    await dp.start_polling(bot, crs=c, usr=users)
 
 if __name__ == "__main__":
     asyncio.run(main())
